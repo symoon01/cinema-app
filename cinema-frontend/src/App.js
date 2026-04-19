e8b1d1b7420cb1ee5f632b3c829387f008551e00
Szymon <szymongmyz1@vp.pl>
2026-04-19 12:53:24 +0200

    added clerk unauthorized redirect

--- cinema-frontend/src/App.js
+++ cinema-frontend/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { SignedIn, useUser } from "@clerk/clerk-react";
+import { SignedIn, useUser, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -34,14 +34,20 @@ function App() {
 
           {/* Szczegóły seansu – dostępne tylko dla zalogowanych */}
           <Route path="/screening/:id" element={
+            <>
               <SignedIn>
                 <Screening />
               </SignedIn>
+              <SignedOut>
+                <Navigate to="/login" />
+              </SignedOut>
+            </>
             }
           />
 
           {/* Panel administratora – tylko dla zalogowanych z rolą ADMIN */}
           <Route path="/admin" element={
+            <>
               <SignedIn>
               {isAdmin ? (
                 <AdminPanel />
@@ -49,6 +55,10 @@ function App() {
                 <Navigate to="/" />
               )}
               </SignedIn>
+              <SignedOut>
+                <Navigate to="/login" />
+              </SignedOut>
+            </>
             }
             
           />
@@ -58,18 +68,28 @@ function App() {
           <Route path="/register" element={<Register />} />
 
           {/* Moje rezerwacje – dostępne tylko dla zalogowanych */}
-          <Route path="/myreservations" element={              
+          <Route path="/myreservations" element={     
+            <>         
               <SignedIn>
                 <MyReservations />
               </SignedIn>
+              <SignedOut>
+                <Navigate to="/login" />
+              </SignedOut>
+            </>
             }
           />
 
           {/* Profil użytkownika – dostępne tylko dla zalogowanych */}
           <Route path="/profile" element={              
+              <>
               <SignedIn>
                 <Profile />
               </SignedIn>
+              <SignedOut>
+                <Navigate to="/login" />
+              </SignedOut>
+              </>
             }
           />
         </Routes>
